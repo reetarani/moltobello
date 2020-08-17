86131c95b4cc9ec50b6d92c763ab9e0f7e71124b
reetarani <reetarani.2009@gmail.com>
2020-08-17 14:23:23 +0530

    first upload

---
+++ moltobello/moltobello/wp-content/themes/startit-child/startit-child/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+/*** Child Theme Function  ***/
+
+function qode_pitch_child_theme_enqueue_scripts() {
+	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+	wp_enqueue_style( 'childstyle' );
+}
+add_action('wp_enqueue_scripts', 'qode_pitch_child_theme_enqueue_scripts', 11);
